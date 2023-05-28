314c7fcab181a83f6003f24405ecd0979dee6b39
Andrea Bocci <andrea.bocci@cern.ch>
2023-05-28 22:27:24 +0300

    Clean up sizeof_alignas

--- hands-on/memory/sizeof_alignof/sizeof_alignof.cc
+++ hands-on/memory/sizeof_alignof/sizeof_alignof.cc
@@ -7,96 +7,76 @@
 #include <typeinfo>
 #include <vector>
 
-#include <cxxabi.h>
+// user-defined types
 
-template <typename T>
-class c_buffer {
-
-public:
-  c_buffer(T * b) :
-    buffer(b) 
-  { }
-
-  ~c_buffer() {
-    free((void *) buffer);
-    buffer = 0;
-  }
-
-  T & operator*() {
-    return * buffer;
-  }
-
-  T * operator->() {
-    return buffer;
-  }
-
-private:
-  T * buffer;
+struct Mixed {
+  int i;
+  short s;
 };
 
-template <typename T>
-void print_size(const char * type) {
-  std::cout << "sizeof(" << type << "): " << sizeof(T) << " bytes, alignof(" << type << "): " << alignof(T) << " bytes" << std::endl;
-}
-
-template <typename T>
-void print_size(void) {
-  int status;
-  const char * type = c_buffer<const char>( abi::__cxa_demangle(typeid(T).name(), 0, 0, &status) ).operator->();
-  print_size<T>(type);
-}
-
-#define PRINT_SIZE(TYPE)    print_size<TYPE>(#TYPE)
-
-struct XYZ {
-  float x;
-  float y;
-  float z;
+struct alignas(16) Wide {
+  int i;
+  short s;
 };
 
+
+// template function and macro to print type information
+
+template <typename T>
+void print_info(const char * type) {
+  std::cout << "  sizeof(" << type << "): " << sizeof(T) << " bytes, alignof(" << type << "): " << alignof(T) << " bytes" << std::endl;
+}
+
+#define PRINT_INFO(TYPE)    print_info<TYPE>(#TYPE)
+
+
+//
 
 int main(void) {
   std::cout << "\ninteger types\n";
-  PRINT_SIZE(bool);
-  PRINT_SIZE(char);
-  PRINT_SIZE(short);
-  PRINT_SIZE(int);
-  PRINT_SIZE(long);
-  PRINT_SIZE(long long);
+  PRINT_INFO(bool);
+  PRINT_INFO(char);
+  PRINT_INFO(short);
+  PRINT_INFO(int);
+  PRINT_INFO(long);
+  PRINT_INFO(long long);
 #if defined __x86_64__
-  PRINT_SIZE(__int128);
+  PRINT_INFO(__int128);
 #else
   std::cout << "\n";
 #endif
 
   std::cout << "\nother standard types\n";
-  PRINT_SIZE(size_t);
-  PRINT_SIZE(uintptr_t);
-  PRINT_SIZE(intptr_t);
+  PRINT_INFO(size_t);
+  PRINT_INFO(uintptr_t);
+  PRINT_INFO(intptr_t);
 
   std::cout << "\nfloating point types\n";
-  PRINT_SIZE(float);
-  PRINT_SIZE(double);
-  PRINT_SIZE(long double);
+  PRINT_INFO(float);
+  PRINT_INFO(double);
+  PRINT_INFO(long double);
 
   std::cout << "\npointer types\n";
-  PRINT_SIZE(void *);
+  PRINT_INFO(void *);
 
   std::cout << "\nSTL types\n";
-  PRINT_SIZE(std::atomic_flag);
-  PRINT_SIZE(std::atomic_bool);
-  PRINT_SIZE(std::atomic_int);
-  PRINT_SIZE(std::chrono::nanoseconds);
-  PRINT_SIZE(std::vector<int>);
-  PRINT_SIZE(std::list<int>);
-  PRINT_SIZE(std::string);
+  PRINT_INFO(std::atomic_flag);
+  PRINT_INFO(std::atomic_bool);
+  PRINT_INFO(std::atomic_int);
+  PRINT_INFO(std::chrono::nanoseconds);
+  PRINT_INFO(std::vector<int>);
+  PRINT_INFO(std::list<int>);
+  PRINT_INFO(std::string);
 
   std::cout << "\nstructs\n";
-  PRINT_SIZE(XYZ);
+  PRINT_INFO(Mixed);
+  PRINT_INFO(Wide);
 
   std::cout << "\narrays\n";
-  PRINT_SIZE(int[4]);
-  PRINT_SIZE(XYZ[4]);
+  PRINT_INFO(int[4]);
+  PRINT_INFO(double[4]);
+  PRINT_INFO(Mixed[4]);
+  PRINT_INFO(Wide[4]);
 
   std::cout << "\n";
 }
