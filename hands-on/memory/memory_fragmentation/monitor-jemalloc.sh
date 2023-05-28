4fd7a0a3fb0940befccf94dad9d891abfe9abb60
Andrea Bocci <andrea.bocci@cern.ch>
2023-05-28 17:47:59 +0300

    Add an exercise on memory fragmentation

---
+++ hands-on/memory/memory_fragmentation/monitor-jemalloc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2 ./heap-fragmentor &
+PID=$!
+
+while [ -d /proc/$PID/ ]; do
+  VSS=$(grep '^VmSize:' /proc/$PID/status | awk '{print $2}')
+  RSS=$(grep '^VmRSS:' /proc/$PID/status | awk '{print $2}')
+  printf "  VSS: %10d kB, RSS: %10d kB\n" $VSS $RSS
+  #PSS=$(grep '^Pss:' /proc/$PID/smaps_rollup | awk '{print $2}')
+  #printf "  VSS: %10d kB, RSS: %10d kB, PSS: %10d kB\n" $VSS $RSS $PSS
+  sleep 0.1s
+done
